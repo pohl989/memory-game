a097d02a60c86058c5ddf861ed99bea70ce354a2
pohl989 <pohl989@gmail.com>
2018-08-26 16:08:08 -0600

    Adding my images and initial thoughts

---
+++ app.js
@@ -0,0 +1,3 @@
+console.log("Javascript is loaded")
+
+var cards = ['adoreDelano', 'alyssaEdwards', 'jigglyCaliente', 'jinxMonsoon', 'jujubee', 'kimChi', 'missFame', 'raja', 'raven', 'sharronNeedles', 'sheaCoulee', 'violetChachki']

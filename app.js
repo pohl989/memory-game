810f7c652969f4478380e3cc258f837ea487b3e5
pohl989 <pohl989@gmail.com>
2018-08-26 20:44:53 -0600

    images are flipping correct

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 $(document).ready(function() {
   //Varables
   var gameArea = $('#gameArea')
-  var listOfQueens = ['adoreDelano', 'alyssaEdwards', 'jigglyCaliente', 'jinxMonsoon', 'jujubee', 'kimChi', 'missFame', 'raja', 'raven', 'sharronNeedles', 'sheaCoulee', 'violetChachki', 'adoreDelano', 'alyssaEdwards', 'jigglyCaliente', 'jinxMonsoon', 'jujubee', 'kimChi', 'missFame', 'raja', 'raven', 'sharronNeedles', 'sheaCoulee', 'violetChachki']
+  // var listOfQueens = ['adoreDelano', 'alyssaEdwards', 'jigglyCaliente', 'jinxMonsoon', 'jujubee', 'kimChi', 'missFame', 'raja', 'raven', 'sharronNeedles', 'sheaCoulee', 'violetChachki', 'adoreDelano', 'alyssaEdwards', 'jigglyCaliente', 'jinxMonsoon', 'jujubee', 'kimChi', 'missFame', 'raja', 'raven', 'sharronNeedles', 'sheaCoulee', 'violetChachki']
+  
+  //easy list
+  var listOfQueens = ['adoreDelano', 'alyssaEdwards', 'jigglyCaliente', 'adoreDelano', 'alyssaEdwards', 'jigglyCaliente']
+  var pick = [];
+  var pickId = [];
+
 
   // Using Durstenfeld shuffle algorithm.
   function shuffleArray(array) {
@@ -15,23 +21,65 @@ $(document).ready(function() {
 
   function addCardToGameArea(array) {
     for (i = 0; i < array.length; i++) {
-      gameArea.append('<div class="card"><div class="card-image ' + array[i] + '"></div></div>')
+      var data = 'data-name="' + array[i] + '"'
+      gameArea.append('<div class="card"' + data + 'id="card-' + i + '" ><div class="card-image ' + array[i] + '"></div></div>')
     }
 
   }
 
   function puttingAllTheCardsOnTheTable(listOfQueens) {
-    //list of Queens
-
-    //This is a random list of queens. I'll use this for the cards
+    //Shuffle list of cards
     shuffleArray(listOfQueens)
-
-    // create cards
-    // inCart.append('<div class="card"><div class="card-image' + adoreDelano + '"></div></div>')
+    // put cards on table
     addCardToGameArea(listOfQueens)
     //put cards on the table
-
   }
+  // generating the cards for the game
   puttingAllTheCardsOnTheTable(listOfQueens)
+
+  // Update pick variables 
+  function updatePicks(choice, id) { 
+    pick.length == 2 ? pick = [choice] : pick.push(choice); 
+    pickId.length == 2 ? pickId = [id] : pickId.push(id); 
+  }
+
+  // remove cards if match
+  function removeIfMatched() {
+    //check for match & make sure they don't click the same card
+    if (pick[0] === pick[1] && pickId[0] !== pickId[1]) {
+      $('#' + pickId[0]).addClass('correct')
+      $('#' + pickId[1]).addClass('correct')
+    } else {
+      setTimeout( function() {
+        $('#' + pickId[0]).delay( 800 ).removeClass('flip')
+        $('#' + pickId[1]).delay( 800 ).removeClass('flip')
+      }, 800);
+
+    }
+  }
+
+  $('.card').on('click', function() {
+    var choice = $(this).data('name')
+    var choiceId = $(this).attr('id')
+    var correct = $(this).hasClass('correct')
+
+    $(this).addClass('flip')
+    console.log(correct)
+    //set pick array variables if card has not flipped
+    correct === false ? updatePicks(choice, choiceId) : null
+
+
+    console.log(pick)
+    console.log(pickId)
+    if (pick.length == 2) {
+      removeIfMatched()
+    }  else {
+      //allow for a second pick
+    }
+
+
+
+    // cart.slideToggle(300);
+  })
 
 });

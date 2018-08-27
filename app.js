568b4c50bc448e30de7238010584c7ec20e432ed
pohl989 <pohl989@gmail.com>
2018-08-26 22:20:17 -0600

    Added bootstrap and you can select difficulty

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 $(document).ready(function() {
   //Varables
   var gameArea = $('#gameArea')
-  // var listOfQueens = ['adoreDelano', 'alyssaEdwards', 'jigglyCaliente', 'jinxMonsoon', 'jujubee', 'kimChi', 'missFame', 'raja', 'raven', 'sharronNeedles', 'sheaCoulee', 'violetChachki', 'adoreDelano', 'alyssaEdwards', 'jigglyCaliente', 'jinxMonsoon', 'jujubee', 'kimChi', 'missFame', 'raja', 'raven', 'sharronNeedles', 'sheaCoulee', 'violetChachki']
+
+  var easy = ['adoreDelano', 'alyssaEdwards', 'jigglyCaliente', 'adoreDelano', 'alyssaEdwards', 'jigglyCaliente']
+  var medium = ['jinxMonsoon', 'jujubee', 'kimChi', 'missFame', 'raja', 'raven', 'sharronNeedles', 'sheaCoulee', 'violetChachki', 'jinxMonsoon', 'jujubee', 'kimChi', 'missFame', 'raja', 'raven', 'sharronNeedles', 'sheaCoulee', 'violetChachki']
+  var hard = ['adoreDelano', 'alyssaEdwards', 'jigglyCaliente', 'jinxMonsoon', 'jujubee', 'kimChi', 'missFame', 'raja', 'raven', 'sharronNeedles', 'sheaCoulee', 'violetChachki', 'adoreDelano', 'alyssaEdwards', 'jigglyCaliente', 'jinxMonsoon', 'jujubee', 'kimChi', 'missFame', 'raja', 'raven', 'sharronNeedles', 'sheaCoulee', 'violetChachki']
   
   //easy list
-  var listOfQueens = ['adoreDelano', 'alyssaEdwards', 'jigglyCaliente', 'adoreDelano', 'alyssaEdwards', 'jigglyCaliente']
+  var listOfQueens, attempts
   var pick = [];
   var pickId = [];
+  var attemptMessage = $('#attempts')
+
 
 
   // Using Durstenfeld shuffle algorithm.
@@ -24,18 +29,23 @@ $(document).ready(function() {
       var data = 'data-name="' + array[i] + '"'
       gameArea.append('<div class="card"' + data + 'id="card-' + i + '" ><div class="card-image ' + array[i] + '"></div></div>')
     }
-
   }
 
-  function puttingAllTheCardsOnTheTable(listOfQueens) {
-    //Shuffle list of cards
-    shuffleArray(listOfQueens)
-    // put cards on table
-    addCardToGameArea(listOfQueens)
-    //put cards on the table
+  //finding the difficulty of the selected
+  function getDifficulty() {
+    var difficulty = $('#difficulty option:selected').text()
+    switch(difficulty) {
+      case 'Easy':
+        listOfQueens = easy 
+        break
+      case 'Medium':
+        listOfQueens = medium
+        break
+      case 'Hard':
+        listOfQueens = hard
+        break
+    }
   }
-  // generating the cards for the game
-  puttingAllTheCardsOnTheTable(listOfQueens)
 
   // Update pick variables 
   function updatePicks(choice, id) { 
@@ -53,33 +63,46 @@ $(document).ready(function() {
       setTimeout( function() {
         $('#' + pickId[0]).delay( 800 ).removeClass('flip')
         $('#' + pickId[1]).delay( 800 ).removeClass('flip')
+        attempts = attempts + 1
+        attemptMessage.text("Failed Attempts: " + attempts)
       }, 800);
 
     }
   }
 
-  $('.card').on('click', function() {
-    var choice = $(this).data('name')
-    var choiceId = $(this).attr('id')
-    var correct = $(this).hasClass('correct')
 
-    $(this).addClass('flip')
-    console.log(correct)
-    //set pick array variables if card has not flipped
-    correct === false ? updatePicks(choice, choiceId) : null
+  $('#startGame').on('click', function(){
+    attempts = 0
+    //Ask for number of cards 
+    getDifficulty()
+    // hide start 
+    $('#startContainer').addClass('d-none')
+    $('#attempts').removeClass('d-none')
 
+    //Shuffle list of cards
+    shuffleArray(listOfQueens)
+    // put cards on table
+    addCardToGameArea(listOfQueens)
+    //put cards on the table
 
-    console.log(pick)
-    console.log(pickId)
-    if (pick.length == 2) {
-      removeIfMatched()
-    }  else {
-      //allow for a second pick
-    }
-
-
-
-    // cart.slideToggle(300);
+    $('.card').on('click', function() {
+      var choice = $(this).data('name')
+      var choiceId = $(this).attr('id')
+      var correct = $(this).hasClass('correct')
+      console.log('Clicked card')
+  
+      $(this).addClass('flip')
+      console.log(correct)
+      //set pick array variables if card has not flipped
+      correct === false ? updatePicks(choice, choiceId) : null
+  
+      if (pick.length == 2) {
+        removeIfMatched()
+      }  else {
+      }
+    })
   })
+
+
 
 });
